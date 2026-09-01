320f8ceaa84e9daa00ffab2bc6a10ad373310280
stasabakumov-sys <stas.abakumov@gmail.com>
2026-09-01 11:38:17 +1000

    Configure PrimeNG Aura theme

--- angular-app/src/app/app.config.ts
+++ angular-app/src/app/app.config.ts
@@ -1,10 +1,24 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withHashLocation } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
+    provideAnimationsAsync(),
     provideRouter(routes, withHashLocation()),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+          cssLayer: false,
+        },
+      },
+    }),
   ],
 };
